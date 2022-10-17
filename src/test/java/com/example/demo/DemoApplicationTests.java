20a7a2b063678ac3c730af50685ac00ff6efb575
kowsalyaDatasirpi <107665616+kowsalyaDatasirpi@users.noreply.github.com>
2022-10-17 23:08:59 +0530

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -11,17 +11,4 @@ class DemoApplicationTests {
 	}
 
 }
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class sampletestTest {
-
-    @Test
-    void addtwonum(){
-
-        sampletest sumofnum = new sampletest();
-        assertEquals(4, sumofnum.add(2, 2));
-    }
-
-}
