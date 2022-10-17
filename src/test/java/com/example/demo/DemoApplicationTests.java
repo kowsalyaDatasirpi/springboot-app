b59fc95ae74339e8a5152e4039ec09b518ae9743
kowsalyaDatasirpi <107665616+kowsalyaDatasirpi@users.noreply.github.com>
2022-10-17 23:02:21 +0530

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoApplicationTests {
+//@SpringBootTest
+//class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
+
+//}
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class sampletestTest {
+
+    @Test
+    void addtwonum(){
+
+        sampletest sumofnum = new sampletest();
+        assertEquals(4, sumofnum.add(2, 2));
+    }
 
 }
