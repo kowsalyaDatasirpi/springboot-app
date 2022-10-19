366b2b406c85db1cb4f24f6163dcd7a2e78e97f6
kowsalyaDatasirpi <107665616+kowsalyaDatasirpi@users.noreply.github.com>
2022-10-19 12:00:27 +0530

    Create sampleTestTest.java

---
+++ src/test/java/com/example/demo/sampleTestTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class sampleTestTest {
+
+    @Test
+    void addtwonum(){
+
+        sampletest sumofnum = new sampletest();
+        assertEquals(4, sumofnum.add(2, 2));
+    }
+
+}
